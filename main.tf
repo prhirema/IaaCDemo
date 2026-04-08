23d063e5c32810282f525df244f0110522b682cb
prhirema <162546046+prhirema@users.noreply.github.com>
2026-04-08 11:25:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.small"
+  subnet_id     = "subnet-056a7f61"
+  key_name      = "UCD_PLUGINS"
+
+  tags = {
+    Name = "VMTerraformDemo"
+  }
+  
+  associate_public_ip_address = false
+  security_groups = ["sg-061a63a3f20fb92c5"]
+}
