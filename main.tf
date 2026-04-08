fc177caf54c0c549ba43c8769337dc9e6e06385e
prhirema <162546046+prhirema@users.noreply.github.com>
2026-04-08 14:35:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "example" {
   key_name      = "UCD_PLUGINS"
 
   tags = {
-    Name = "IAACTerraformDemo-testing12"
+    Name = "IAACTerraformDemo-testing123"
   }
   
   associate_public_ip_address = false
